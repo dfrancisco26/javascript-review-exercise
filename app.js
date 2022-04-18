7f9a3ea6a77e3c71be7b6ad16088805031fda901
David Francisco <dfrancisco2677@gmail.com>
2022-04-18 16:23:45 -0700

    Adds getFormattedName function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // These functions are tested by console logging - all the console log statements
 // should print true.
 
@@ -7,7 +8,7 @@
 // the number of seconds
 
 function convertToSeconds(numOfMinutes) {
-    return (numOfMinutes * 60)
+    return (numOfMinutes * 60);
 }
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
@@ -16,7 +17,7 @@ console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
 function areaOfTriangle(base, height) {
-    return (base * height / 2)
+    return (base * height / 2);
 }
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
@@ -24,7 +25,7 @@ console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 // Given 2 numbers, return true if their sum is less than 100
 // return false if it is greater than 100
 function lessThan100(num1, num2) {
-    return (num1+num2 < 100)
+    return (num1 + num2 < 100);
 }
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
 console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`);
@@ -34,7 +35,14 @@ console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`
 // that accepts a first_name and last_name as required parameters
 // and a third parameter called middle_name which is optional.
 // The function should return a string with the first, middle and last names
-function getFormattedName() {}
+function getFormattedName(first_name, last_name, middle_name = '') {
+    if (middle_name === '') {
+        return first_name + ' ' + last_name;
+    } else {
+        return first_name + ' ' + middle_name + ' ' + last_name;
+    }
+    
+}
 console.log(
     `getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg': ${
         getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg'
@@ -56,7 +64,10 @@ console.log(
 //    that checks if the year is a leap year, and returns the correct
 //    number of days for Feb. The default value for the year should be
 //    the current year
+
 function daysInMonth() {}
+
+
 console.log(`daysInMonth(2) === 29: ${daysInMonth(2) === 29}`);
 console.log(`daysInMonth(3) === 31: ${daysInMonth(3) === 31}`);
 console.log(`daysInMonth(11) === 30: ${daysInMonth(11) === 30}`);
