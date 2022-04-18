e3121197635c667b6781da0cc306c1a780ed55d4
David Francisco <dfrancisco2677@gmail.com>
2022-04-18 14:12:07 -0700

    Adds areaOfTriangle function

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
+function areaOfTriangle(base, height) {
+    return (base * height / 2)
+}
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
